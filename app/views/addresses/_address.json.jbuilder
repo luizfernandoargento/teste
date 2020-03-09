1338fc1268b9b995ddc43d7935df3c3a528ce9c2
Fernando <luizfernandoargento@gmail.com>
2020-03-09 15:48:05 -0300

    Criados 'member' e 'address'

---
+++ app/views/addresses/_address.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! address, :id, :member_id, :tipo, :postal_code, :state, :city, :district, :street_name, :number, :complement, :created_at, :updated_at
+json.url address_url(address, format: :json)
---
+++ db/migrate/20200309184646_create_addresses.rb
@@ -0,0 +1,17 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.integer :member_id
+      t.string :tipo
+      t.string :postal_code
+      t.string :state
+      t.string :city
+      t.string :district
+      t.string :street_name
+      t.integer :number
+      t.string :complement
+
+      t.timestamps
+    end
+  end
+end
